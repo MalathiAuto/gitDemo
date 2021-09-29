85d185154b3bb73b25cae1803da21738b640dd3e
MalathiAuto <malt5sree@gmail.com>
2021-09-29 16:47:24 -0400

    added somthing to arry project

--- src/arun/array.java
+++ src/arun/array.java
@@ -5,7 +5,7 @@ public class array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] myintarray = {100,31,26,40,52};
+		int[] myintarray = {50,31,26,40,52};
 		
 				for(int i = 0; i < myintarray.length;i++) {
 			System.out.println(myintarray[i]);
